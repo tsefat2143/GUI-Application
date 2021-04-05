976f2273f9e09a2371f01b05d947df248e0f60f9
tsefat2143 <tajdidul.sefat@gmail.com>
2021-04-04 23:45:07 -0400

    Simple Tkinter GUI

---
+++ GUI.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+class GUI():
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.quitButton = Button(frame, text='Quit', command=frame.quit)
+        self.quitButton.pack(side=RIGHT)
+
+
+root = Tk()
+myGUI = GUI(root)
+root.title('Insurance Application')
+root.mainloop()
